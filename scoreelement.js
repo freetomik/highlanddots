2ed94ef0204dbeee98567bee721320e80cdd0575
Jeremy.Starcher <Jeremy.Starcher@9519ae4e-59f2-5971-2688-1d9bd1fb6a9e>
2010-04-24 04:23:42 +0000

    Trunk fully merged now.. all working.

--- scoreelement.js
+++ scoreelement.js
@@ -4,6 +4,7 @@ function ScoreElement() {
   this.isPrintable = false;
   this.c = {};                  // Storage area for some commonly used calcuations.
   this.paddingRight = staff.details.space/2;
+
   return this;  
 }
 
