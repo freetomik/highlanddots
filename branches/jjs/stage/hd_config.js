fead8a514c3f5650c9956db6a08036c319c01188
Jeremy.Starcher <Jeremy.Starcher@9519ae4e-59f2-5971-2688-1d9bd1fb6a9e>
2010-04-29 19:33:18 +0000

    Minor updates on the config

--- branches/jjs/stage/hd_config.js
+++ branches/jjs/stage/hd_config.js
@@ -7,240 +7,223 @@ function doIt() {
   hdots_prefs.registerPlugin("s_engine", "long", "The Long Version", undefined);  
   
   hdots_prefs.makeHdConfigForm();
-  alert(hdots_prefs.getValueOf("color1"));
 }
 
 
 var hdots_prefs = (function() {
-                   var hdConfigData = [];
-                   
-                   function getOptionByName(n) {
-                     var o;
-                     for(var i = 0, l = hdConfigData.length; i < l; i++) {
-                       o = hdConfigData[i];
-                       if (o.name === n) {return o}
-                     }
-                   }
-                   
-                   function getValueOf(n) {
-                     o = getOptionByName(n);
-                     if (o) {
-                       if (o.value) {return o.value;}
-                       return o.def;
-                     }
-                   }
-                   
-                   
-                   function addConfigOption(o) {
-                     hdConfigData.push(o);                 
-                   }
-                   
-                   function prepConfig() {
-                     var o;
-                     
-                     o = {
-                       type: "select",
-                       label: "Note color",
-                       name: "color1",
-                       def:  "blue",
-                       options: [
-                         {"#FF0000": "red"},
-                         {"#0000FF": "blue"},
-                         {"#00FF00": "green"}
-                       ],
-                     };
-                     
-                     addConfigOption(o);
-                     
-                     o = {
-                       type: "boolean",
-                       name: "outlines",
-                       label: "Draw outlines?",
-                       def: true
-                     }
-                     
-                     addConfigOption(o);
-                     
-                     o = {
-                       type: "plugin",
-                       label: "Spacing Engine",
-                       name: "s_engine",
-                       def:  "",
-                       options: [],
-                     };
-                     
-                     addConfigOption(o);
-                     
-                     
-                     
-                   }
-                   
-                   function makeHdConfigForm() {
-                     var k, v;
-                     var FORMNAME = "hd_config";
-                     
-                     function addToForm(el) {
-                       var target = API.getEBI(FORMNAME);
-                       
-                       var label = API.createElement("label");
-                       API.addElementText(label, v.label + ": ");
-                       label.appendChild(el);
-                       el.name = "hd_" + v.name;
-                       
-                       
-                       target.appendChild(label);
-                       target.appendChild(API.createElement("br"));
-                     }
-                     
-                     function makeBoolean() {
-                       var el = API.createElementWithProperties('input', { type:'checkbox', checked:v.def });
-                       addToForm(el);    
-                     }
-                     
-                     function makeSelect() {
-                       var el = API.createElement("select");
-                       var i, l, o = {}, opt;
-                       var o;
-                       opt = v.options;
-                       
-                       
-                       for (i = 0, l = opt.length; i < l; i++) {
-                         o = opt[i];
-                         API.forEachProperty(o, function(p) {
-                                             API.addOption(el, p, o[p]);
-                                             if (p === v.def) { el.selectedIndex = i; }
-                                             
-                         });
-                         
-                       }
-                       addToForm(el);
-                     }
-                     
-                     
-                     function getDataFromForm() {
-                       
-                     }
-                     
-                     function makeAcceptButton() {
-                       var target = API.getEBI(FORMNAME);
-                       var el = API.createElement("button");
-                       API.addElementText(el, "Accept");
-                       API.attachListener(el, 'click', function() {
-                                          window.alert('Clicked!');
-                                          API.cancelPropagation(el);
-                                          
-                                          return API.cancelDefault(el);
-                       });  
-                       target.appendChild(el);
-                       
-                     }
-                     
-                     for(var i = 0, l = hdConfigData.length; i < l; i++) {
-                       v = hdConfigData[i];
-                       switch (v.type) {
-                       case "select":
-                       case "plugin":
-                         makeSelect();
-                         break;
-                       case "boolean":
-                         makeBoolean();
-                         break;
-                       }
-                     }
-                     makeAcceptButton();
-                   }
-                   
-                   function registerPlugin(optionName, pluginName, desc, fun) {
-                   var o = getOptionByName(optionName);
-                   var o1 = {}
-                   o1[pluginName] = desc;
-                   o.options.push(o1);
-                   }
-                   
-                   return {
-                     registerPlugin: registerPlugin, 
-                     getValueOf: getValueOf,
-                     prepConfig: prepConfig, 
-                     makeHdConfigForm: makeHdConfigForm
-                   };
+    var hdConfigData = [];
+    var formContentsId = "hd_config";
+    formId = formContentsId + "_form"; 
+    
+    function getOptionByName(n) {
+      var o;
+      for(var i = 0, l = hdConfigData.length; i < l; i++) {
+        o = hdConfigData[i];
+        if (o.name === n) {return o}
+      }
+    }
+    
+    function getValueOf(n) {
+      o = getOptionByName(n);
+      if (o) {
+        if (o.value) {return o.value;}
+        return o.def;
+      }
+    }
+    
+    
+    function addConfigOption(o) {
+      hdConfigData.push(o);                 
+    }
+    
+    function prepConfig() {
+      var o;
+      
+      o = {
+        type: "select",
+        label: "Note color",
+        name: "color1",
+        def:  "blue",
+        options: [
+        {"#FF0000": "red"},
+        {"#0000FF": "blue"},
+        {"#00FF00": "green"}
+        ],
+      };
+      
+      addConfigOption(o);
+      
+      o = {
+        type: "boolean",
+        name: "outlines",
+        label: "Draw outlines?",
+        def: true
+      }
+      
+      addConfigOption(o);
+      
+      o = {
+        type: "plugin",
+        label: "Spacing Engine",
+        name: "s_engine",
+        def:  "",
+        options: [],
+      };
+      
+      addConfigOption(o);
+      
+      
+      
+    }
+    
+    function makeHdConfigForm() {
+      var k, v;
+      
+      function addToForm(el) {
+        var target = API.getEBI(formContentsId);
+        
+        var label = API.createElement("label");
+        API.addElementText(label, v.label + ": ");
+        label.appendChild(el);
+        el.name = "hd_" + v.name;
+        
+        
+        target.appendChild(label);
+        target.appendChild(API.createElement("br"));
+      }
+      
+      function makeBoolean() {
+        var el = API.createElementWithProperties('input', { type:'checkbox', checked:v.def });
+        addToForm(el);    
+      }
+      
+      function makeSelect() {
+        var el = API.createElement("select");
+        var i, l, o = {}, opt;
+        var o;
+        opt = v.options;
+        
+        
+        for (i = 0, l = opt.length; i < l; i++) {
+          o = opt[i];
+          API.forEachProperty(o, function(p) {
+              API.addOptions(el, o);
+              if (p === v.def) { el.selectedIndex = i; }
+              
+          });
+          
+        }
+        addToForm(el);
+      }
+      
+      
+      function getDataFromForm() {
+        var s = API.HD_serializeFormUrl(API.getEBI(formId));
+        alert(JSON.stringify(s));
+      }
+      
+      function makeAcceptButton() {
+        var target = API.getEBI(formContentsId);
+        var el = API.createElement("button");
+        API.addElementText(el, "Accept");
+        API.attachListener(el, 'click', function() {
+            getDataFromForm();
+            API.cancelPropagation(el);
+            
+            return API.cancelDefault(el);
+        });  
+        target.appendChild(el);
+        
+      }
+      
+      for(var i = 0, l = hdConfigData.length; i < l; i++) {
+        v = hdConfigData[i];
+        switch (v.type) {
+        case "select":
+        case "plugin":
+          makeSelect();
+          break;
+        case "boolean":
+          makeBoolean();
+          break;
+        }
+      }
+      makeAcceptButton();
+    }
+    
+    function registerPlugin(optionName, pluginName, desc, fun) {
+      var o = getOptionByName(optionName);
+      var o1 = {}
+      o1[pluginName] = desc;
+      o.options.push(o1);
+    }
+    
+    
+    /*
+    Augment the standard 'My Library' .. eventually, this code should go into the
+    tools section to be re-used later.
+    */
+    
+    if (API.getOptionValue) {
+      var serializeFormUrl = function(f) {
+        var e, // form element
+        n, // form element's name
+        t, // form element's type
+        o, // option element
+        es = f.elements,
+        c = {}; // Hold the values from the form.
+        
+        var reCheck = new RegExp('^(checkbox|radio)$');
+        var reText = new RegExp('^(text|password|hidden|textarea)$');
+        
+        function add(n, v) {
+          c[n] =v;
+        }
+        
+        for (var i=0, ilen=es.length; i<ilen; i++) {
+          e = es[i];
+          n = e.name;
+          if (n && !e.disabled) {
+            t = e.type;
+            if (!t.indexOf('select')) {
+              // The 'select-one' case could reuse 'select-multiple' case
+              // The 'select-one' case code is an optimization for
+              // serialization processing time.
+              if (t == 'select-one' || e.multiple === false) {
+                if (e.selectedIndex >= 0) {
+                  add(n, API.getOptionValue(e.options[e.selectedIndex]));
+                }
+              }
+              else {
+                for (var j = 0, jlen = e.options.length; j < jlen; j++) {
+                  o = e.options[j];
+                  if (o.selected) {
+                    add(n, API.getOptionValue(o));
+                  }
+                }
+              } 
+            }
+            else if (reCheck.test(t)) {
+              if (e.checked) {
+                add(n, e.value || 'on');
+              }          
+            }
+            else if (reText.test(t)) {
+              add(n, e.value);
+            }
+          }
+        }
+        return c; //.join('&');
+      };
+      API.HD_serializeFormUrl = serializeFormUrl;
+    }
+    
+    return {
+      registerPlugin: registerPlugin, 
+      getValueOf: getValueOf,
+      prepConfig: prepConfig, 
+      makeHdConfigForm: makeHdConfigForm
+    };
+    
+    
 }());
-
-
-/**
-*
-*  URL encode / decode
-*  http://www.webtoolkit.info/
-*
-**/
-
-var Url = {
-  
-  // public method for url encoding
-  encode : function (string) {
-    return escape(this._utf8_encode(string));
-  },
-  
-  // public method for url decoding
-  decode : function (string) {
-    return this._utf8_decode(unescape(string));
-  },
-  
-  // private method for UTF-8 encoding
-  _utf8_encode : function (string) {
-    string = string.replace(/\r\n/g,"\n");
-    var utftext = "";
-    
-    for (var n = 0; n < string.length; n++) {
-      
-      var c = string.charCodeAt(n);
-      
-      if (c < 128) {
-        utftext += String.fromCharCode(c);
-      }
-      else if((c > 127) && (c < 2048)) {
-        utftext += String.fromCharCode((c >> 6) | 192);
-        utftext += String.fromCharCode((c & 63) | 128);
-      }
-      else {
-        utftext += String.fromCharCode((c >> 12) | 224);
-        utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-        utftext += String.fromCharCode((c & 63) | 128);
-      }
-      
-    }
-    
-    return utftext;
-  },
-  
-  // private method for UTF-8 decoding
-  _utf8_decode : function (utftext) {
-    var string = "";
-    var i = 0;
-    var c = c1 = c2 = 0;
-    
-    while ( i < utftext.length ) {
-      
-      c = utftext.charCodeAt(i);
-      
-      if (c < 128) {
-        string += String.fromCharCode(c);
-        i++;
-      }
-      else if((c > 191) && (c < 224)) {
-        c2 = utftext.charCodeAt(i+1);
-        string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-        i += 2;
-      }
-      else {
-        c2 = utftext.charCodeAt(i+1);
-        c3 = utftext.charCodeAt(i+2);
-        string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-        i += 3;
-      }
-      
-    }
-    
-    return string;
-  }
-  
-}
