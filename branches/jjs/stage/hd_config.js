961aaa581afdcfabcac7db380cac8cedec1c5237
Jeremy.Starcher <Jeremy.Starcher@9519ae4e-59f2-5971-2688-1d9bd1fb6a9e>
2010-04-29 03:45:30 +0000

    Beginning work on options system.

---
+++ branches/jjs/stage/hd_config.js
@@ -0,0 +1,196 @@
+var hdConfigData = [];
+
+
+function doIt() {
+  prepConfig();
+  makeHdConfigForm();
+}
+
+function prepConfig() {
+  var o;
+  
+  o = {
+    type: "select",
+    label: "Note color",
+    name: "color1",
+    def:  "blue",
+    options: [
+    {"#FF0000": "red"},
+    {"#0000FF": "blue"},
+    {"#00FF00": "green"}
+    ],
+  };
+  
+  hdConfigData.push(o);
+  
+  o = {
+    type: "boolean",
+    name: "outlines",
+    label: "Draw outlines?",
+    def: true
+  }
+  
+  hdConfigData.push(o);
+  
+  
+}
+
+function makeHdConfigForm() {
+  var k, v;
+  var FORMNAME = "hd_config";
+  
+  function addToForm(el) {
+    var target = API.getEBI(FORMNAME);
+    
+    var label = API.createElement("label");
+    API.addElementText(label, v.label + ": ");
+    label.appendChild(el);
+    el.name = "hd_" + v.name;
+    
+    
+    target.appendChild(label);
+    target.appendChild(API.createElement("br"));
+  }
+  
+  function makeBoolean() {
+    var el = API.createElementWithProperties('input', { type:'checkbox', checked:v.def });
+    addToForm(el);    
+  }
+  
+  function makeSelect() {
+    var el = API.createElement("select");
+    var i, l, o = {}, opt;
+    var o;
+    opt = v.options;
+    
+    
+    for (i = 0, l = opt.length; i < l; i++) {
+      o = opt[i];
+      API.forEachProperty(o, function(p) {
+                          API.addOption(el, p, o[p]);
+                          if (p === v.def) { el.selectedIndex = i; }
+                          
+      });
+      
+    }
+    addToForm(el);
+  }
+  
+  
+  function getDataFromForm() {
+    
+  }
+  
+  function makeAcceptButton() {
+    var target = API.getEBI(FORMNAME);
+    var el = API.createElement("button");
+    API.addElementText(el, "Accept");
+    API.attachListener(el, 'click', function() {
+                       window.alert('Clicked!');
+                       API.cancelPropagation(el);
+                       
+                       return API.cancelDefault(el);
+    });  
+    target.appendChild(el);
+    
+  }
+  
+  for (k in hdConfigData) {
+    if (hdConfigData.hasOwnProperty(k)) {
+      v = hdConfigData[k];
+      switch (v.type) {
+      case "select":
+        makeSelect();
+        break;
+      case "boolean":
+        makeBoolean();
+        break;
+      }
+    }
+  }
+  
+  makeAcceptButton();
+  
+}
+
+
+
+
+/**
+*
+*  URL encode / decode
+*  http://www.webtoolkit.info/
+*
+**/
+
+var Url = {
+  
+	// public method for url encoding
+	encode : function (string) {
+		return escape(this._utf8_encode(string));
+	},
+  
+	// public method for url decoding
+	decode : function (string) {
+		return this._utf8_decode(unescape(string));
+	},
+  
+	// private method for UTF-8 encoding
+	_utf8_encode : function (string) {
+		string = string.replace(/\r\n/g,"\n");
+		var utftext = "";
+    
+		for (var n = 0; n < string.length; n++) {
+      
+			var c = string.charCodeAt(n);
+      
+			if (c < 128) {
+				utftext += String.fromCharCode(c);
+			}
+			else if((c > 127) && (c < 2048)) {
+				utftext += String.fromCharCode((c >> 6) | 192);
+				utftext += String.fromCharCode((c & 63) | 128);
+			}
+			else {
+				utftext += String.fromCharCode((c >> 12) | 224);
+				utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+				utftext += String.fromCharCode((c & 63) | 128);
+			}
+      
+		}
+    
+		return utftext;
+	},
+  
+	// private method for UTF-8 decoding
+	_utf8_decode : function (utftext) {
+		var string = "";
+		var i = 0;
+		var c = c1 = c2 = 0;
+    
+		while ( i < utftext.length ) {
+      
+			c = utftext.charCodeAt(i);
+      
+			if (c < 128) {
+				string += String.fromCharCode(c);
+				i++;
+			}
+			else if((c > 191) && (c < 224)) {
+				c2 = utftext.charCodeAt(i+1);
+				string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+				i += 2;
+			}
+			else {
+				c2 = utftext.charCodeAt(i+1);
+				c3 = utftext.charCodeAt(i+2);
+				string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+				i += 3;
+			}
+      
+		}
+    
+		return string;
+	}
+  
+}
