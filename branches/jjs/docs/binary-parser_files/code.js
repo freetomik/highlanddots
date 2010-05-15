f132ea73d7f9266dc3b8fb3e87deb33d8f82bd49
Jeremy.Starcher <Jeremy.Starcher@9519ae4e-59f2-5971-2688-1d9bd1fb6a9e>
2010-05-15 00:43:53 +0000

    Fresh start

--- branches/jjs/docs/binary-parser_files/code.js
+++
@@ -1,6 +0,0 @@
-function code(m){
-	addEvent(document.forms.formVote, "submit", function(e){
-		for(var c, f = this.rate, i = f.length; i-- && !f[i].checked;);
-		return i > 0 || (alert(m), false);
-	});
-}
