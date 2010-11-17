e805e2adcd4cd4621f43877d203e71c993179d55
pipemakertjm <pipemakertjm@9519ae4e-59f2-5971-2688-1d9bd1fb6a9e>
2010-11-17 18:07:46 +0000

    Branch for integrating MIDI playback

---
+++ branches/midi/examples.js
@@ -0,0 +1,225 @@
+var bwwExamples = (function() 
+                   {
+                   var tunes = {};
+                   var tune;
+                   
+                   // Tabs have been changed to ~ to prevent mangling by
+                   // editors.  They are changed back when needed.
+                   
+                   tune = [
+                     "Bagpipe Reader:1.0",
+                     "MIDINoteMappings,(54,56,58,59,61,63,64,66,68,56,58,60,61,63,65,66,68,70,55,57,59,60,62,64,65,67,69)",
+                     "FrequencyMappings,(370,415,466,494,554,622,659,740,831,415,466,523,554,622,699,740,831,932,392,440,494,523,587,659,699,784,880)",
+                     "InstrumentMappings,(71,71,45,33,1000,60,70)",
+                     "GracenoteDurations,(30,40,30,50,100,200,800,1200,250,250,250,500,200)",
+                     "FontSizes,(90,100,88,80,250)",
+                     "TuneFormat,(1,0,M,L,500,500,500,500,P,0,0)",
+                     "",
+                     "TuneTempo,80",
+                     "",
+                     "\"The 10th H.L.I. Crossing The Rhine\",(T,L,0,0,Times New Roman,16,700,0,0,18,0,0,0)",
+                     "\"March\",(Y,C,0,0,Times New Roman,14,400,0,0,18,0,0,0)",
+                     "\"\",(M,R,0,0,Times New Roman,14,400,0,0,18,0,0,0)",
+                     "\"Highland Dots unit test\",(F,R,0,0,Times New Roman,12,400,0,0,18,0,0,0)",
+                     "",
+                     "",
+                     "& sharpf sharpc 6_8 I!'' gg LA_8",
+                     "!       thrd Dr_8 'd Fl_16 gg LAl_8             thrd D_4        F_8", 
+                     "!       gg Fr_8 'f HGl_16 HAl_8~~~hdbf Fr_8 'f El_16 Dl_8",
+                     "!       gg C_4                  grp E_8         gg LAr_8 'la dg Cl_16 El_8",
+                     "!       gg Er_8 'e Fl_16 HGl_8~~        hdbf Fr_8 'f El_16 Dl_8         !t",
+                     "",
+                     "& sharpf sharpc '22",
+                     "!       lgstd Dr_8 'd Fl_16 gg LAl_8             thrd D_4        F_8",
+                     "!       gg Fr_8 'f HGl_16 HAl_8  ~               hdbf Fr_8 'f El_16 Dl_8",
+                     "!       gg C_4                  grp E_8         gg LAr_8 'la dg Cl_16 El_8",
+                     "!       thrd D_4 'd                             lgstd D_4 _'                     ''!I",
+                     "",
+                     "& sharpf sharpc I!'' dbhg HG_8",
+                     "!        dbha HA_4                    F_8             thrd D_4                F_8",
+                     "!       gg Fr_16 HAl_8 'ha Fl_8  ~         dbf Fr_8 'f El_16 Dl_8",
+                     "!       gg C_4                  grp E_8         gg LAr_8 'la dg Cl_16 El_8",
+                     "!       gg Er_8 'e Fl_16 HGl_8      ~        hdbf Fr_8 'f El_16 Dl_8         !t",
+                     "",
+                     "& sharpf sharpc '1",
+                     "!       dbha HA_4                    F_8             thrd D_4                F_8",
+                     "!       gg Fr_16 HAl_8 'ha Fl_8    ~        dbf Fr_8 'f El_16 Dl_8",
+                     "!       gg C_4                  grp E_8         gg LAr_8 'la dg Cl_16 El_8",
+                     "!       thrd D_4 'd                             ~lgstd D_4 _'                     ''!I",
+                     "",
+                     "& sharpf sharpc I!'' gg LA_8",
+                     "!       thrd D_4 'd                             gg LAr_8 'la Dl_16 Fl_8",
+                     "!       gg Fr_16 HAl_8 'ha Fl_8~~dbf Fr_8 'f El_16 Dl_8",
+                     "!       dbc C_4 'c                              gg LAr_8 'la dg Cl_16 El_8",
+                     "!       gg Er_8 'e Fl_16 HGl_8  ~~ hdbf Fr_8 'f El_16 Dl_8         !t",
+                     "",
+                     "& sharpf sharpc", 
+                     "!       lgstd D_4 'd                             gg LAr_8 'la Dl_16 Fl_8",
+                     "!       gg Fr_16 HAl_8 'ha Fl_8 ~~dbf Fr_8 'f El_16 Dl_8",
+                     "!       gg C_4                 grp E_8         gg LAr_8 'la dg Cl_16 El_8",
+                     "!       thrd D_4 'd                             lgstd D_4                     ''!I",
+                     "",
+                     "& sharpf sharpc I!'' dbhg HG_8",
+                     "!       HA_4                    grp HA_8        LAr_8 'la Dl_16 Fl_8",
+                     "!       gg Fr_16 HAl_8 'ha Fl_8  ~dbf Fr_8 'f El_16 Dl_8",
+                     "!       gg C_4                  grp E_8         gg LAr_8 'la dg Cl_16 El_8",
+                     "!       gg Er_8 'e Fl_16 HGl_8~     hdbf Fr_8 'f El_16 Dl_8         !t",
+                     "",
+                     "& sharpf sharpc '1",
+                     "!       dbha HA_4                    grp HA_8        LAr_8 'la Dl_16 Fl_8",
+                     "!       gg Fr_16 HAl_8 'ha Fl_8 ~~dbf Fr_8 'f El_16 Dl_8",
+                     "!       gg C_4                  grp E_8         gg LAr_8 'la dg Cl_16 El_8",
+                     "!       thrd D_4 'd                             lgstd D_4 _'                     ''!I",
+                     "",
+                     "",
+                     "& sharpf sharpc '2",
+                     "!       lgstd Dr_8 'd Fl_16 gg LAl_8             thrd D_4        F_8",
+                     "!       gg Fr_8 'f HGl_16 HAl_8  ~               hdbf Fr_8 'f El_16 Dl_8",
+                     "!       gg C_4                  grp E_8         gg LAr_8 'la dg Cl_16 El_8",
+                     "!       thrd D_4 'd                             lgstd D_4 _'                     ''!I",
+                     ""
+                   ];
+                   
+                   tunes["The 10th H.L.I. Crossing The Rhine"] = tune;
+                   
+                   
+                   tune = [
+                     "Bagpipe Reader:1.0",
+                     "MIDINoteMappings,(55,57,59,60,62,64,65,67,69,57,59,61,62,64,66,67,69,71,56,58,60,61,63,65,66,68,70)",
+                     "FrequencyMappings,(392,440,494,523,587,659,699,784,880,440,494,554,587,659,740,784,880,988,415,466,523,554,622,699,740,831,932)",
+                     "InstrumentMappings,(71,71,46,34,1000,60,70)",
+                     "GracenoteDurations,(30,40,30,50,100,200,800,1200,250,250,250,500,200)",
+                     "FontSizes,(90,100,100,80,250)",
+                     "TuneFormat,(1,0,M,L,500,500,500,500,P,0,0)",
+                     "",
+                     "TuneTempo,84",
+                     "",
+                     "\"Mairi's Wedding\",(T,L,0,0,Times New Roman,16,700,0,0,18,0,0,0)",
+                     "\"March\",(Y,C,0,0,Times New Roman,14,400,0,0,18,0,0,0)",
+                     "\"Traditional\",(M,R,0,0,Times New Roman,14,400,0,0,18,0,0,0)",
+                     "\"Highland Dots unit test\",(F,R,0,0,Times New Roman,12,400,0,0,18,0,0,0)",
+                     "",
+                     "",
+                     "& sharpf sharpc  2_4~",
+                     "I!~gg LA_4  brl~LAr_8 'la Bl_16",
+                     "!~thrd Dr_8  El_8~dbf F_4",
+                     "!~dbe Er_8 Dl_8~~dbb Br_8 Dl_8",
+                     "! ~dbf Fr_8 El_8 ~~dbf Fr_8 HAl_8",
+                     "!t",
+                     "",
+                     "& sharpf sharpc~ !~gg LA_4  brl~LAr_8 'la Bl_16",
+                     "!~thrd Dr_8  El_8~dbf F_4",
+                     "!~dbe Er_8 Dl_8~~dbb Br_8 eg LGl_8",
+                     "!~gg LA_4~~~brl LA_4", 
+                     "''!I",
+                     "",
+                     "& sharpf sharpc~",
+                     "I!~gg Fr_8 HAl_8~~gg HA_4",
+                     "!~grp HAr_8 'ha HGl_16~dbf F_4",
+                     "!~dbe Er_8 Dl_8~~dbb Br_8 Dl_8",
+                     "! ~dbf Fr_8 El_8 ~    '1~dbf F_4",
+                     "!t",
+                     "",
+                     "& sharpf sharpc   !~ gg Fr_8 HAl_8~gg HA_4",
+                     "!~grp HAr_8 'ha HGl_16~dbf F_4",
+                     "!~dbe Er_8 Dl_8~~dbb Br_8 eg LGl_8",
+                     "!~gg LA_4~~~brl LA_4 _'",
+                     "''!I",
+                     "",
+                     "& sharpf sharpc  '2~dbf Fr_8 HAl_8",
+                     "! gg LA_4  brl~LAr_8 'la Bl_16",
+                     "!~thrd Dr_8  El_8~dbf F_4",
+                     "!~dbe Er_8 Dl_8~~dbb Br_8 eg LGl_8",
+                     "!~gg LA_4~~~brl LA_4 _'",
+                     "''!I",
+                     ""
+                   ];
+                   tunes["Mairi's Wedding"] = tune;
+
+                   tune = [
+                     "Bagpipe Reader:1.0",
+                     "MIDINoteMappings,(55,57,59,60,62,64,65,67,69,57,59,61,62,64,66,67,69,71,56,58,60,61,63,65,66,68,70)",
+                     "FrequencyMappings,(392,440,494,523,587,659,699,784,880,440,494,554,587,659,740,784,880,988,415,466,523,554,622,699,740,831,932)",
+                     "InstrumentMappings,(71,71,46,34,1000,60,70)",
+                     "GracenoteDurations,(30,40,30,50,100,200,800,1200,250,250,250,500,200)",
+                     "FontSizes,(90,100,100,80,250)",
+                     "TuneFormat,(1,0,M,L,500,500,500,500,P,0,0)",
+                     "",
+                     "TuneTempo,84",
+                     "",
+                     "\"Tune With Errors\",(T,L,0,0,Times New Roman,16,700,0,0,18,0,0,0)",
+                     "\"March\",(Y,C,0,0,Times New Roman,14,400,0,0,18,0,0,0)",
+                     "\"Traditional\",(M,R,0,0,Times New Roman,14,400,0,0,18,0,0,0)",
+                     "\"Highland Dots unit test\",(F,R,0,0,Times New Roman,12,400,0,0,18,0,0,0)",
+                     "",
+                     "",
+                     "& sharpf flatc  2_4~",
+                     "I!~gg LA_4  brl~LAr_8 'la Bl_16",
+                     "!~thrd Dr_8  El_8~dbf F_4",
+                     "!~dbe Er_8 Dl_8~~dbb Br_8 Dl_8",
+                     "! ~dbf Fr_8 El_8 ~~dbf Fr_8 HAl_8",
+                     "!t",
+                     "",
+                     "& sharpf sharpc~ !~gg LA_4  brl~LAr_8 'la Bl_16",
+                     "!~thrd Dr_8  El_8~dbf F_4",
+                     "!~dbe Er_8 Dl_8~~dbb Br_8 eg LGl_8",
+                     "!~gg LA_4~~~brl LA_4", 
+                     "''!I",
+                     "",
+                     "& sharpf flatd~",
+                     "I!~gg Fr_8 HAl_8~~gg HA_4",
+                     "!~grp HAr_8 'ha HGl_16~dbf F_4",
+                     "!~dbe Er_8 testwidget Dl_8~~dbb Br_8 Dl_8",
+                     "! ~dbf Fr_8 El_8 ~    '1~dbf F_4",
+                     "!t",
+                     "",
+                     "& sharpf sharpc   !~ gg Fr_8 HAl_8~gg HA_4",
+                     "!~grp HAr_8 'ha HGl_16~dbf F_4",
+                     "!~dbe Er_8 Dl_8~~dbb Br_8 eg LGl_8",
+                     "!~gg LA_4~~~brl LA_4 _'",
+                     "''!I",
+                     "",
+                     "& sharpf sharpc  '2~dbf Fr_8 HAl_8",
+                     "! gg LA_4  brl~LAr_8 'la Bl_16",
+                     "!~thrd Dr_8  El_8~dbf F_4",
+                     "!~dbe Er_8 Dl_8~~dbb Br_8 eg LGl_8",
+                     "!~gg LA_4~~~brl LA_4 _'",
+                     "''!I",
+                     ""
+                   ];
+                   tunes["Tune with Errors"] = tune;
+                   
+                   
+                   tune = [
+                     tunes["Mairi's Wedding"].join(" ")
+                   ];
+                   tunes["Testing: All on one line"] = tune;
+                   
+                   function makeOptions(element) {
+                     var size = 0;
+                     API.forEachProperty(tunes, function(el, i) {
+                                         API.addOption(element, i);
+                                         size++;
+                     });
+                     element.size = size;
+                     
+                   }
+                   
+                   function loadData(el) {
+                     
+                     var idx = el.selectedIndex;
+                     var o = el.options[idx].text;
+                     var s = tunes[o].join("\r\n");
+                     s = s.replace("~", "\t", "g");
+                     return s;
+                   }
+                   
+                   return {
+                     makeOptions: makeOptions,
+                     loadData: loadData
+                   };
+                   
+                   }
+                   )
+();
+
---
+++ branches/midi/postimport.js
@@ -0,0 +1,254 @@
+"use strict";
+
+
+var HD_TO_MIDINAME_XLATE;
+var MIDINAME_TO_HD_XLATE;
+var MIDINAME_TO_MIDI_NOTE;
+
+// Prep the tables that are used to convert Highland Dots internal format
+// to standard MIDI.
+(function() {
+ HD_TO_MIDINAME_XLATE = {};
+ MIDINAME_TO_HD_XLATE = {};
+ MIDINAME_TO_MIDI_NOTE = {};
+ var octave = "c,c#,d,d#,e,f,f#,g,g#,a,a#,b".split(',');
+ 
+ var sharpToFlat = {
+   "c#": "db",
+   "d#": "eb",
+   "f#": "gb"
+ };
+ 
+ 
+ var idx = 0;
+ var i;
+ var noteName;
+ 
+ var hdName;
+ var midiName;
+ 
+ var l = octave.length;
+ var midiOctaveNumber = -1; // Below actual value because we inc the octave below
+ var HdOctaveNumber = -4;
+ 
+ 
+ function setVals() {
+   hdName = noteName + HdOctaveNumber;
+   midiName = noteName + midiOctaveNumber;
+   
+   HD_TO_MIDINAME_XLATE[hdName] = midiName;
+   MIDINAME_TO_HD_XLATE[midiName] = hdName;
+   MIDINAME_TO_MIDI_NOTE[midiName] = i;
+ }
+ 
+ for (i = 0; i < 127; i++) {
+   noteName = octave[i % l];
+   
+   if (noteName === "c") {midiOctaveNumber++;}
+   if (noteName === "a") {HdOctaveNumber++;}
+   
+   setVals();
+   if (sharpToFlat[noteName]) {
+     noteName = sharpToFlat[noteName];
+     setVals();
+   }
+   
+ }
+ 
+ 
+}
+()); 
+
+//alert(MIDINAME_TO_HD_XLATE.toSource());
+
+
+
+
+/* Since BWW and ABC will have certain aspects of being imported in common,
+handle them here rather than maintaining two separate versions in the
+import sections
+*/
+
+function postImport(score) {
+  
+  // Shift the note by the key signature and set the MIDI value
+  function finalizeNote() {
+    if (mel.type === "melody" || mel.type === "gracenote") {
+      var note = mel.staffPosition.substring(0,1); // Staff position never has sharps or flats.
+      var midiOffset = +mel.staffPosition.substring(1);
+      if (lastKeySig && lastKeySig.data && lastKeySig.data.key && lastKeySig.data.key[note]) {
+        note += lastKeySig.data.key[note]
+      }
+      mel.midiName = HD_TO_MIDINAME_XLATE[note + midiOffset];
+      mel.midiNote = MIDINAME_TO_MIDI_NOTE[mel.midiName];
+      mel.shortNoteName = note;
+    }
+  }
+  
+  
+  function gatherKeySig() {
+    // This routine is used to figure out the time sig after a BWW import.
+    // Any sharps or flats that appear directly after the clef determines
+    // the key sig.
+    var ii;
+    var mel;
+    
+    var note;
+    var modes = {
+      sharp: "#",
+      flat: "b",
+      natural: "-"
+    };
+    
+    var data = {};
+    
+    var keyMel;
+    
+    for (ii = i+1; i < l; ii++) {
+      mel = score.data[ii];
+      note = undefined;
+      
+      if (mel.type === "beat") {continue;}  // Skip any stray beat marks
+      if (mel.type === "keysig") {
+        keyMel = mel;
+        continue;
+      }
+      
+      if (modes[mel.name]) {
+        if (typeof data.type === "undefined") {
+          data.keyType = mel.name;
+          data.count = 0;
+          data.key = {};                    
+        } else {
+          if (data.keyType !== mel.name) {
+            alert("A key signature can contain sharps or flats but not both.");
+          }
+        }
+        score.data[ii] = score.createNullElement(); // Remove from the score
+        
+        data.count++;
+        note = mel.staffNote.split('')[0];
+        logit("note = " + note);
+        data.key[note] = modes[mel.name];
+        continue;
+      }
+      
+      if (keyMel) {
+        keyMel.data = data;
+        lastKeySig = keyMel;
+      }
+      // Nothing left to catch
+      logit(data);
+      break;
+    }
+  }
+  
+  
+  var i, l, mel;
+  var beatUnit;
+  var beatsPerBar;
+  
+  var volta = undefined;
+  var tupletMelodyMels = undefined; // Save off the tuplets and recalculate them at the end.
+  var tupletLength = 0;
+  var measureLength = 0;
+  var lineLength = 0;
+  var measureCount = 1; // People like 1-based, not zero based.
+  var lastNewBar; 
+  var maxMeasuresInLine
+  var lineMeasureNumber = 0;
+  var staffLine = 1;
+  var lastKeySig;
+     
+  for (i = 0, l = score.data.length; i < l; i++) {
+    mel = score.data[i];
+    if (!mel) {continue;}
+    if (!mel.type) {continue;}
+    
+    if (mel.name === "treble-clef") {
+      gatherKeySig();
+      if (!lastKeySig.data.key) {
+        lastKeySig.data = {
+          keyType:"sharp", count:1, key:{f:"#", c:"#"},
+          // The note values have already been adjusted to play properly when
+          // the key sig is not given. This makes an "incorrect" BWW file,
+          // but they do exist.
+          ignoreIfUsingBwwNoteValues: true 
+        };
+      }   
+    }
+        
+    if (mel.type === "timesig") {
+      beatUnit = mel.beatUnit; 
+      beatsPerBar = mel.beatsPerBar;
+    }
+    
+    if (mel.type === "phrasegroup" && mel.collectionName === "voltas") {
+      if (mel.sectionStart) { volta = mel; }
+      if (mel.sectionEnd) { volta = undefined; }
+    }
+    
+    if (mel.type === "phrasegroup" && mel.collectionName === "triplets") {
+      if (mel.sectionStart) {
+        tupletMelodyMels = [];
+        tupletLength = 0;
+      }
+      if (mel.sectionEnd) {
+        // FIXME: This needs to calculate the /adjusted/ length of tuplets.
+        tupletMelodyMels = undefined;
+        
+      }
+    }
+    
+    if (mel.type === "staffControl") {
+      if (mel.newBar) {
+        if (measureLength && lastNewBar) { 
+          lastNewBar.measureNumber = measureCount;
+          lastNewBar.str = [staffLine, lineMeasureNumber].join(":"); 
+          lastNewBar.beatsPerBar = beatsPerBar;
+          lastNewBar.measureLength = measureLength;
+          if (measureLength !== beatsPerBar) {
+            if (lineMeasureNumber === 0) {
+              lastNewBar.isLeadIn = true;
+            } else {
+              lastNewBar.isLeadOut = true;
+            }
+          }
+          lineMeasureNumber++;
+          measureCount++;
+        }
+        //mel.measureNumber = measureLength // DEBUG;
+        mel.measureLength = measureLength;
+        measureLength = 0;
+        lastNewBar = mel;
+      }
+      
+      if (mel.staffEnd) {
+        maxMeasuresInLine = Math.max(maxMeasuresInLine, lineMeasureNumber);
+        staffLine++;
+        mel.lineLength = lineLength;
+        lineMeasureNumber = 0;
+        lineLength = 0;
+      }
+    }
+    
+    // Calculate proper lengths
+    if (mel.type === "melody") {
+      mel.beatFraction = beatUnit/mel.duration; 
+      
+      //TODO: Handle hold-cut note lengths properly.
+      
+      if (mel.dotType === "dot") { mel.beatFraction *= 1.5; }
+      if (mel.dotType === "doubledot") { mel.beatFraction *= 1.75; }
+      measureLength += mel.beatFraction;
+      lineLength += mel.beatFraction;
+      
+      if (tupletMelodyMels) { tupletLength += mel.beatFraction; }
+      if (volta) { mel.repeatOnPasses = volta.repeatOnPasses;}
+    }
+    
+    finalizeNote();    
+    //lastKeySig
+    
+  }
+}
